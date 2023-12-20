1d2ca65adc764c8a8aabd584040c372efe423513
Hisham Balboul <hishamsabry-97@hotmail.com>
2023-12-20 13:59:00 -0800

    fix DB issue

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""This module instantiates astorage object"""
+"""This module instantiates a storage object"""
 from os import getenv
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
 
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
