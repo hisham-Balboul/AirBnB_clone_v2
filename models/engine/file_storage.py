ebab82d2e16bb3a4e6b2b56116450bcaac3c3622
Hisham Balboul <hishamsabry_97@hotmail.com>
2024-01-25 07:58:24 -0800

    web_flask Task 6

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -60,8 +60,12 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """Delete a given object from __objects, if it exists."""
+        """Delete a given object from __objects, if it exists"""
         try:
             del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
         except (AttributeError, KeyError):
             pass
+    
+    def close(self):
+        """Call the reload method"""
+        self.reload()
