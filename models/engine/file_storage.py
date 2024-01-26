0a1b5b4d3ae4b797e0fbf0a3843e1583bb8a36e2
Hisham Balboul <hishamsabry_97@hotmail.com>
2024-01-26 06:10:34 -0800

    fix task 7

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,7 +65,7 @@ class FileStorage:
             del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
         except (AttributeError, KeyError):
             pass
-    
+
     def close(self):
         """Call the reload method"""
         self.reload()
